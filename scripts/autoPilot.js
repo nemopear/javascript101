9b6d2020dd734061c5558b19a5975775d735c72d
Nutchaya Vorayingyong <nv@manaosoftware.com>
2016-10-04 15:32:07 +0700

    Add math Random

--- scripts/autoPilot.js
+++ scripts/autoPilot.js
@@ -1,7 +1,16 @@
-﻿$(document).ready(function () {
+﻿function calRandom() {
     $('#playPause').click(function () {
-        var randomX;
-        var randomY;
-
+        var bodyWidth = $('body').width();
+        var bodyHeight = $('body').height();
+        var randomX = Math.random() * bodyWidth;
+        var randomY = Math.random() * bodyHeight;
+        console.log(randomX);
+        $('.ball').css('top', randomY);
+        $('.ball').css('left', randomX);
     });
+};
+
+
+$(document).ready(function () {
+    calRandom();
 });
