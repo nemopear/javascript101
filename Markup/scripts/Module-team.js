5fda6eeeb3bbb50834c8be0934ca7d325fdfd1b4
nutchaya@opendream.co.th <nutchaya@opendream.co.th>
2016-11-04 07:47:42 +0700

    add event touchstart

--- Markup/scripts/Module-team.js
+++ Markup/scripts/Module-team.js
@@ -2,16 +2,28 @@
     setTimeout(function () { truncateText('.staffNameHover'); }, 200);
 });
 
-$('.staffItem').on('click', function () {
-    $(this).addClass('active');
-    $(this).click(function () {
-        $(this).removeClass('active');
-    });
+
+var eventName = ('ontouchstart' in window) ? 'click' : 'mouseenter mouseleave';
+
+$('.staffItem').on(eventName, function () {
+	$(this).toggleClass('active');
+	var checkActive = $(this).hasClass('active');
+	console.log(checkActive);
+	$('.staffItem').removeClass('active');
+	if (checkActive) {
+		$(this).addClass('active');
+	}
 });
 
+// $('body').on('click', function (event) {
+// 	var element = event.target;
+// });
 
-$('.staffItem').on('mouseover', function () {
-    $(this).addClass('active');
-}).on('mouseout', function () {
-    $(this).removeClass('active');
-});
+
+
+
+// $('.staffItem').on('mouseover', function () {
+//     $(this).addClass('active');
+// }).on('mouseout', function () {
+//     $(this).removeClass('active');
+// });
